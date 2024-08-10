f95735fb0feb0d6330562ec10ada58d70cfd9bd6
Abbos29 <abbosvaliev2003@gmail.com>
2024-08-10 21:01:48 +0500

    feat: header + hero

---
+++ build/js/scripts.js
@@ -0,0 +1,18 @@
+// Custom Scripts
+// Custom scripts
+
+
+
+// burger menu
+
+const menu = document.querySelector('.nav__menu')
+const burger = document.querySelector('.nav__burger')
+
+
+burger.addEventListener('click', () => {
+    menu.classList.toggle('active')
+})
+
+
+
+
